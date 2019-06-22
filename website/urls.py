b78bf227009b93c3755d8f90e1ace585cc4e6526
tnoff <tylernorth18@gmail.com>
2019-06-21 17:02:31 -0700

    Use random string for admin page

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('lhnoq7ziwzk3jisw482wd9p50mt2lq58/', admin.site.urls),
 ]
