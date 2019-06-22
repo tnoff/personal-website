5643856b7be2c26655a3a35ade8e35666178ac84
tnoff <tylernorth18@gmail.com>
2019-06-21 17:35:41 -0700

    add skeleton of main page

--- website/settings.py
+++ website/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -38,7 +40,9 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from homepage import views
+
 urlpatterns = [
     path('lhnoq7ziwzk3jisw482wd9p50mt2lq58/', admin.site.urls),
+    path('', views.home_page),
 ]
