3a8679755c9a8c1e84e004577fddcb4a729e3c75
tnoff <1326564+tnoff@users.noreply.github.com>
2020-01-11 03:35:38 +0000

    Be extra sure with models

--- my_calendar/models.py
+++ my_calendar/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=255, unique=True, blank=False)
+    name = models.CharField(max_length=255, unique=True, blank=False, null=False)
 
     def __str__(self):
         return self.name
 
 
 class Birthday(models.Model):
-    month = models.IntegerField(blank=False)
-    day = models.IntegerField(blank=False)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE, blank=False)
+    month = models.IntegerField(blank=False, null=False)
+    day = models.IntegerField(blank=False, null=False)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, blank=False, null=False)
 
     def __str__(self):
         return '%s - %s.%s' % (self.person, self.month, self.day)
 
 class Task(models.Model):
-    month_offset = models.IntegerField(blank=False)
-    week_offset = models.IntegerField(blank=False)
-    day_of_week = models.IntegerField(blank=False)
-    message = models.CharField(max_length=1024, blank=False)
-    due_date = models.DateField(blank=False)
+    month_offset = models.IntegerField(blank=False, null=False)
+    week_offset = models.IntegerField(blank=False, null=False)
+    day_of_week = models.IntegerField(blank=False, null=False)
+    message = models.CharField(max_length=1024, blank=False, null=False)
+    due_date = models.DateField(blank=False, null=False)
 
     def __str__(self):
         return self.message
