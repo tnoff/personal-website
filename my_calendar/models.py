8842fd7ed41ade0d27b825cd24f6a774a08571ba
tnoff <1326564+tnoff@users.noreply.github.com>
2020-01-11 03:29:58 +0000

    Actual do not allow blank models

--- my_calendar/models.py
+++ my_calendar/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=255, unique=True, null=False)
+    name = models.CharField(max_length=255, unique=True, blank=False)
 
     def __str__(self):
         return self.name
 
 
 class Birthday(models.Model):
-    month = models.IntegerField(null=False)
-    day = models.IntegerField(null=False)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE, null=False)
+    month = models.IntegerField(blank=False)
+    day = models.IntegerField(blank=False)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, blank=False)
 
     def __str__(self):
         return '%s - %s.%s' % (self.person, self.month, self.day)
 
 class Task(models.Model):
-    month_offset = models.IntegerField(null=False)
-    week_offset = models.IntegerField(null=False)
-    day_of_week = models.IntegerField(null=False)
-    message = models.CharField(max_length=1024, null=False)
+    month_offset = models.IntegerField(blank=False)
+    week_offset = models.IntegerField(blank=False)
+    day_of_week = models.IntegerField(blank=False)
+    message = models.CharField(max_length=1024, blank=False)
     due_date = models.DateField(blank=False)
 
     def __str__(self):
