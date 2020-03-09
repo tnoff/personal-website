a98e3c7a40cd141f87da34d52cfb0fbeb6e813b5
tnoff <1326564+tnoff@users.noreply.github.com>
2020-03-08 21:06:27 -0700

    Make sure phone number can be null

--- my_calendar/models.py
+++ my_calendar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False, null=False)
     birthday = models.DateField(blank=True, null=True)
-    phone_number = models.CharField(max_length=32)
+    phone_number = models.CharField(max_length=32, blank=True, null=True)
 
     def __str__(self):
         return self.name
