506a61963fe81980ba69b5916901ef4d8633640d
tnoff <1326564+tnoff@users.noreply.github.com>
2020-04-05 20:07:21 +0000

    Add chown commands to copied secrets in gunicorn config

--- files/usr/local/bin/gunicorn-start.sh
+++ files/usr/local/bin/gunicorn-start.sh
@@ -3,4 +3,6 @@
 cp /secret/website/secret_key /opt/website/
 cp /secret/website/my.cnf /opt/website/
 
+chown -R www-data: /opt/website/secret_key /opt/website/my.cnf
+
 /usr/local/bin/gunicorn -c /etc/gunicorn.conf --chdir /opt/website/ website.wsgi:application
