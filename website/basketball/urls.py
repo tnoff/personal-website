6d7b1055408356ea64230ba9c4afda17228f718a
tnoff <1326564+tnoff@users.noreply.github.com>
2020-06-20 22:14:02 +0000

    Add views for single teams

--- website/basketball/urls.py
+++ website/basketball/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, re_path
 from basketball import views
 
 basketball_urls = [
-    path('294be434-0c6c-4118-a286-d4a3dfd3032e/', views.standings),
+    re_path('294be434-0c6c-4118-a286-d4a3dfd3032e/?$', views.standings),
+    re_path('294be434-0c6c-4118-a286-d4a3dfd3032e/(?P<year>\d+)/?$', views.standings),
+    re_path(r'294be434-0c6c-4118-a286-d4a3dfd3032e/(?P<year>\d+)/(?P<short_name>\w+)/?$', views.team_show),
 ]
 
--- website/basketball/views.py
+++ website/basketball/views.py
@@ -1,29 +1,55 @@
 import logging
 
-from django_otp.decorators import otp_required
+from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import render
+from django_otp.decorators import otp_required
 
 from basketball.models import Game, Team
 
 logger = logging.getLogger(__name__)
 
-def _generate_team_data():
+def _generate_team_dict(team):
+    return {
+        'short_name': team.short_name,
+        'name': team.name,
+        'year': team.year,
+        'wins': 0,
+        'losses': 0,
+        'point_differential': 0,
+        'wins_last_eight': 0,
+        'losses_last_eight': 0,
+        'point_differential_last_eight': 0,
+    }
+
+def _generate_team_data(year=None, team=None):
+    '''
+    Generate team data
+
+    team_id     :   If passed, only return for single team
+    '''
+    if not (team or year):
+        raise Exception('Year or team needed to generate data')
+
     logger.debug('Gathering team metadata')
     team_data = {}
-    for team in Team.objects.all():
-        team_data[team.id] = {
-            'short_name': team.short_name,
-            'name': team.name,
-            'wins': 0,
-            'losses': 0,
-            'point_differential': 0,
-            'wins_last_eight': 0,
-            'losses_last_eight': 0,
-            'point_differential_last_eight': 0,
-        }
-    logger.info('Iterating through game data')
+
+    if year:
+        all_teams = Team.objects.filter(year=year)
+    else:
+        all_teams = Team.objects.filter(year=team.year)
+    for team_obj in all_teams:
+        team_data[team_obj.id] = _generate_team_dict(team_obj)
+    
+    # If team passed, only return games with those players
+    if not team:
+        games = Game.objects.filter(Q(away_team__year=year) & Q(home_team__year=year))
+    else:
+        games = Game.objects.filter(Q(away_team=team) | Q(home_team=team))
+
     # Iterate backwards to get last 8 games today
-    for game in Game.objects.all().order_by('-date'):
+    logger.info('Iterating through game data')
+    for game in games.order_by('-date'):
         logger.debug(f'Generating info from game {game.id}')
         away_team = team_data[game.away_team.id]
         home_team = team_data[game.home_team.id]
@@ -63,25 +89,77 @@ def _generate_team_data():
         new_dict = value
         new_dict['id'] = key
         # Add points differntial per game
-        per_game = new_dict['point_differential'] * 1.0 / (new_dict['wins'] + new_dict['losses'])
-        new_dict['point_differential_per_game'] = per_game
-        per_game_eight = new_dict['point_differential_last_eight'] * 1.0 / (new_dict['wins_last_eight'] + new_dict['losses_last_eight'])
-        new_dict['point_differential_last_eight'] = per_game_eight
+        try:
+            per_game = new_dict['point_differential'] * 1.0 / (new_dict['wins'] + new_dict['losses'])
+            new_dict['point_differential_per_game'] = per_game
+        except ZeroDivisionError:
+            new_dict['point_differential_per_game'] = 0.0
+        try:
+            per_game_eight = new_dict['point_differential_last_eight'] * 1.0 / (new_dict['wins_last_eight'] + new_dict['losses_last_eight'])
+            new_dict['point_differential_last_eight'] = per_game_eight
+        except ZeroDivisionError:
+            new_dict['point_differential_last_eight'] = 0.0
         team_data_list.append(new_dict)
-    return team_data_list
+    return team_data_list, games
 
 @otp_required
-def standings(request):
+def standings(request, year=2020):
     '''
     Show Current Standings
     '''
-    team_data = _generate_team_data()
+    team_data, _games = _generate_team_data(year=year)
     team_data = sorted(team_data, key=lambda k: k['point_differential_per_game'], reverse=True)
     for (count, team) in enumerate(team_data):
-        team['standing'] = count
+        team['standing'] = count + 1
         team['point_differential_per_game'] = '%.2f' % team['point_differential_per_game']
         team['point_differential_last_eight'] = '%.2f' % team['point_differential_last_eight']
     view_data = {
         'team_data': team_data,
     }
     return render(request, 'basketball/standings.html', view_data)
+
+@otp_required
+def team_show(request, year, short_name):
+    '''
+    Get team information
+    '''
+    try:
+        team = [i for i in Team.objects.filter(year=year, short_name=short_name)][0]
+    except IndexError:
+        raise Http404(f'Unable to locate team')
+
+    team_data, games = _generate_team_data(team=team)
+    this_team = None
+
+    for team_obj in team_data:
+        if not team_obj['id'] == team.id:
+            continue
+        team_obj['point_differential_per_game'] = '%.2f' % team_obj['point_differential_per_game']
+        team_obj['point_differential_last_eight'] = '%.2f' % team_obj['point_differential_last_eight']
+        this_team = team_obj
+
+    # Generate results per game
+    team_game_list = []
+    for game in reversed(games):
+        if game.away_team.id == this_team['id']:
+            if game.away_score > game.home_score:
+                game.result = "Win"
+            else:
+                game.result = "Loss"
+            game.diff = game.away_score - game.home_score
+        if game.home_team.id == this_team['id']:
+            if game.away_score > game.home_score:
+                game.result = "Loss"
+            else:
+                game.result = "Win"
+            game.diff = game.home_score - game.away_score
+        team_game_list.append(game)
+
+    view_data = {
+        'all_team_data': team_data,
+        'games': team_game_list,
+        'team': team,
+        'team_data': this_team,
+    }
+
+    return render(request, 'basketball/team.html', view_data)
