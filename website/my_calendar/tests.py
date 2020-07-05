3fe9129970308520012d683d7923a948703c1828
tnoff <1326564+tnoff@users.noreply.github.com>
2020-07-05 00:09:38 +0000

    Fix deltas on my calendar tests

--- website/my_calendar/tests.py
+++ website/my_calendar/tests.py
@@ -9,7 +9,8 @@ class MyCalendarTestCase(TestCase):
     def test_tasks_due_date_weekly_past_due(self):
         # With a task due last sunday, make sure marking complete
         # with only a two week offset has correct timedelta
-        last_sunday = date.today()
+        # Make sure to subtract one here in case today is sunday
+        last_sunday = date.today() - timedelta(1)
         while last_sunday.weekday() != 6:
             last_sunday -= timedelta(1)
         task = Task.objects.create(message="test message",
@@ -24,7 +25,8 @@ class MyCalendarTestCase(TestCase):
     def test_tasks_due_date_weekly_not_due(self):
         # With a task due next sunday, make sure marking complete
         # with only a two week offset has correct timedelta
-        next_sunday = date.today()
+        # Make sure to add one here in case today is sunday
+        next_sunday = date.today() + timedelta(1)
         while next_sunday.weekday() != 6:
             next_sunday += timedelta(1)
         task = Task.objects.create(message="test message",
@@ -39,7 +41,8 @@ class MyCalendarTestCase(TestCase):
     def test_tasks_due_date_monthly_paste_due(self):
         # With a task due last sunday, make sure marking complete
         # with a month offset has correct dimensions
-        last_sunday = date.today()
+        # Make sure to subtract one here in case today is sunday
+        last_sunday = date.today() - timedelta(1)
         while last_sunday.weekday() != 6:
             last_sunday -= timedelta(1)
         task = Task.objects.create(message="test message",
