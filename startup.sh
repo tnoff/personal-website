28d1c7871b677f485fc53b0b660e5f9e000cbe02
Tyler North <me@tyler-north.com>
2024-07-29 21:20:13 -0700

    dockerfile updates (#9)

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 set -o errexit
 
 /opt/website-venv/bin/python /opt/website/manage.py collectstatic --no-input
-/opt/website-venv/bin/python /opt/website/manage.py migrate --fake-initial
+# /opt/website-venv/bin/python /opt/website/manage.py migrate --fake-initial
 /opt/website-venv/bin/python /opt/website/manage.py runserver
--- website/settings.py
+++ website/settings.py
@@ -72,12 +72,24 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'POSTGRES_DATABASE' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['POSTGRES_DATABASE'],
+            'USER': os.environ['POSTGRES_USER'],
+            'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+            'HOST': os.environ['POSTGRES_HOST'],
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 LOG_FILE = '/var/log/website/website.log'
 if SECRET_KEY_FILE.exists():
