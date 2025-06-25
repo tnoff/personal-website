62e39332f55d62bc4ffb989bdd11a634bfc9aa4e
Tyler North <me@tyler-north.com>
2025-06-24 21:03:26 -0700

    chore: add django middleware

--- website/settings.py
+++ website/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'website.middleware.OpenTelemetryEnrichMiddleware'
 ]
 
 ROOT_URLCONF = 'website.urls'
