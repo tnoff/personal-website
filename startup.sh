ad0e5592de4e48006daeabfb0653a507ba16ee87
Tyler North <me@tyler-north.com>
2025-11-14 00:44:39 -0800

    chore: remove onebox deploy (#32)

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-if [ -v ONEBOX_DEPLOY ]; then
-    python /opt/web/manage.py migrate
-fi
-
 python /opt/web/manage.py collectstatic --no-input
 python /opt/web/manage.py runserver 0.0.0.0:8000
